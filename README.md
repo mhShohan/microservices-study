a3a321777050f94351a90475e151062ab9f44a1d
mhShohan <mehdihasanshohan25@gmail.com>
2024-10-26 19:12:20 +0600

    microservices study and created inventory and product services

---
+++ README.md
@@ -0,0 +1,12 @@
+# Microservices Practice - video (day-2) timestamp: 02.20.00
+
+# CLI
+
+- docker ps
+- docker inspect container_name
+
+### connect to docker container database
+
+- `postgres` - container name, or
+- `host.docker.internal` , or
+- by using ip address of the container, view the ip address by `docker inspect <container_name>` this command and view the network object
---
+++ services/inventory/src/index.ts
@@ -0,0 +1,71 @@
+import express, { Application, RequestHandler } from 'express'
+import dotenv from 'dotenv'
+import morgan from 'morgan'
+import cors from 'cors'
+import {
+  createInventory,
+  deleteInventory,
+  getAllInventories,
+  getInventoryDetails,
+  getSingleInventoryById,
+  updateInventory
+} from '@/controllers'
+
+const app: Application = express()
+dotenv.config()
+
+app.use(express.json())
+app.use(cors())
+app.use(morgan('dev'))
+
+// environment variables
+const PORT = process.env.PORT || 4002
+const serviceName = process.env.SERVICE_NAME || 'inventory-service'
+
+
+// routes
+app.put('/inventories/:id', updateInventory as RequestHandler)
+app.get('/inventories/:id', getSingleInventoryById as RequestHandler)
+app.delete('/inventories/:id', deleteInventory as RequestHandler)
+app.get('/inventories/:id/details', getInventoryDetails as RequestHandler)
+app.post('/inventories', createInventory as RequestHandler)
+app.get('/inventories', getAllInventories as RequestHandler)
+
+
+// health route
+app.get('/health', (_req, res) => {
+  res.status(200).json({
+    status: 'success',
+    statusCode: 200,
+    success: true,
+    message: `${serviceName} is UP now!`
+  })
+})
+
+// not Found
+app.use((_req, res) => {
+  res.status(404).json({
+    status: 'failure',
+    statusCode: 404,
+    success: false,
+    message: '404! Not Found.'
+  })
+})
+
+// error handler
+app.use((err, _req, res, _next) => {
+  console.log(err.stack)
+
+  res.status(500).json({
+    status: 'failure',
+    statusCode: 500,
+    success: false,
+    message: 'Internal Server Error!',
+    errors: err
+  })
+})
+
+
+app.listen(PORT, () => {
+  console.log(`${serviceName} running on port: ${PORT}`)
+})
